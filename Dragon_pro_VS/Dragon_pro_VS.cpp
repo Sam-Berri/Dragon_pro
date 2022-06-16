68d6d33458df26121060b89bafc7d49fd8faf780
Sam-Berri <107113152+Sam-Berri@users.noreply.github.com>
2022-06-16 13:16:56 +0100

    Vector Class colab Worked!
    brake through

--- Dragon_pro_VS/Dragon_pro_VS.cpp
+++ Dragon_pro_VS/Dragon_pro_VS.cpp
@@ -3,9 +3,16 @@
 #include <iostream>
 #include <vector>
 #include "Header1.h"
-int main()
-{
-   Dragon 
+int main(){
+	
+	Dragon Igneal("Igneal", "Fire", 500, 40, 70, { "Slash", "Crunch" }, { "Blast Breath","Burning Spiral" });
 
+
+	std::cout<< Igneal.Magical_attacks[0];
 }
+
+
+
+
+
 
