3da9188e5be7d02d3d328ddc07653c5a4ccc1293
Sam-Berri <107113152+Sam-Berri@users.noreply.github.com>
2022-06-16 14:41:59 +0100

    both dragon atrributes saved

--- Dragon_pro_VS/Dragon_pro_VS.cpp
+++ Dragon_pro_VS/Dragon_pro_VS.cpp
@@ -4,11 +4,13 @@
 #include <vector>
 #include "Header1.h"
 int main(){
-	
-	Dragon Igneal("Igneal", "Fire", 500, 40, 70, { "Slash", "Crunch" }, { "Blast Breath","Burning Spiral" });
+	                                //
+	Dragon Igneal("Igneal", "Fire", 500, 40, 60, { "Slash", "Crunch" }, { "Blast Breath","Burning Spiral" });
+	Dragon Roknol("Roknol", "Earth", 700, 20, 60, { "Ram","Stomp" }, { "Terraform","Rock Blast" });
+
+	std::cout << Igneal.Magical_attacks[0];
 
 
-	std::cout<< Igneal.Magical_attacks[0];
 }
 
 
